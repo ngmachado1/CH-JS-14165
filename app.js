79eebe5959815c4e577e18622105eb31710f8924
ngmachado1 <54647104+ngmachado1@users.noreply.github.com>
2021-02-24 13:42:21 -0300

    ejercicios resueltos after class 4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // calcular el perimetro de un rectangulo
 
 // 2*(a+b)
+
 function pedirDato(){
     return parseInt(prompt("ingrese el lado"));
 
@@ -10,45 +11,34 @@ function perimetroRectangulo(ladoA, ladoB){
     return  2 * (ladoA + ladoB);
 }
 
-//let ladoA = pedirDato();
-//let ladoB = pedirDato();
+let ladoA = pedirDato();
+let ladoB = pedirDato();
 
-//alert(perimetroRectangulo(ladoA, ladoB));
+alert(perimetroRectangulo(ladoA, ladoB));
 
 
 
 
 // tabla de multiplicar
 
-// var numero = parseFloat(prompt('ingresa el numero que queres multiplicar'));
+ var numero = parseFloat(prompt('ingresa el numero que queres multiplicar'));
 
-// function crearTabla(n){
-//     // contar los numeros del 1 al 10
-//     for(var i = 0; i<=100; i++){
-//         console.log(n +"x"+ i +" = "+i * n);
-
-//         document.write(n +"x"+ i +" = "+i * n);
-//     }
-    
-
-//     // devolver valores
-// }
-// crearTabla(numero);
-
-    var numero = parseInt(prompt('tabla que desea conocer'));
-    function crearTabla(n){//     //multiplicar del 1 al 10
-        
-        document.write("<h1 class='rojo'> esta es la tabla del " + n + "</h1>")
-        
-        
-        for(var i = 0; i<=10; i++){
-            var resultado = n*i;
-            document.write(n + " x " + i + " = " + resultado  + "<br>")
-        }
-    }
-    //crearTabla(numero)
+ function crearTabla(n){
+     //el metodo nativo de js document.write imprime por pantalla lo que le pasemos por parametro ("")
+     //ej, imprimimos el titulo de la tabla de multiplicar
+     document.write("<h1 class='rojo'> esta es la tabla del " + n + "</h1>")
+     
+     // contar los numeros del 1 al 10
+     for(var i = 0; i<=10; i++){
+         
+         //devolver por consola el valor de i en cada ciclo del bucle y multiplicarlo por el parametro de entrada de la funcion
+         console.log(n +"x"+ i +" = "+i * n);
+         
+         //mostrar en el documento html la tabla
+         document.write(n +"x"+ i +" = "+i * n);
+     }
+ }
+//llamamos a la funcion para que se ejecute
+ crearTabla(numero);
 
 
-    setTimeout(crearTabla(numero), 3000);
-
-    //setTimeout(callback , tiempo en milisegundos)
